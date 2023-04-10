8b2d21ad635a69bb3b85d0581df3b291247dc573
Diego-Lopes-Ferreira <dglpferreira@gmail.com>
2023-04-10 18:09:37 -0300

    Lab 04

---
+++ lab04/Etapa02.c
@@ -0,0 +1,174 @@
+#include <P18F4550.h>
+#include <delays.h>
+
+// Configuracoes
+#pragma config FOSC = HS  // Fosc = 20MHz, Tcy = 200ns (PLL nao habilitado)
+                          // sinal de clk do sistema e o oscliador primario
+#pragma config CPUDIV = OSC1_PLL2
+#pragma config PWRT = ON  // Power Up Timer habilitado
+#pragma config WDT = OFF  // Watchdog desativado
+#pragma config LVP = OFF  // Desabilita gravacao em baixa tensao
+
+#define USANDO_PICSIM
+#ifndef USANDO_PICSIM
+#define CC1 LATAbits.LATA5  // Controle Disp 1
+#define CC2 LATAbits.LATA2  // Controle Disp 2
+#define CC3 LATEbits.LATE0  // Controle Disp 3
+#define CC4 LATEbits.LATE2  // Controle Disp 4
+#else
+#define CC1 LATAbits.LATA2  // Controle Disp 1
+#define CC2 LATAbits.LATA3  // Controle Disp 2
+#define CC3 LATAbits.LATA4  // Controle Disp 3
+#define CC4 LATAbits.LATA5  // Controle Disp 4
+#endif
+
+#define DELAY_DOS_LEDS 5  // Delay1KTCYx = 5 * 1000 * 200ns = 1ms
+#define DELAY_DOS_BTNS 5  // Delay1KTCYx = 5 * 1000 * 200ns = 1ms
+#define BTN0 PORTBbits.RB0
+#define BTN1 PORTBbits.RB1
+#define BTN2 PORTBbits.RB2
+
+char const tab[] = {
+    0b00111111,  // 0
+    0b00000110,  // 1
+    0b01011011,  // 2
+    0b01001111,  // 3
+    0b01100110,  // 4
+    0b01101101,  // 5
+    0b01111101,  // 6
+    0b00000111,  // 7
+    0b01111111,  // 8
+    0b01100111   // 9
+};
+
+// 0: solto | 1: pressionado
+char btn0Pressionado = 0;
+char btn1Pressionado = 0;
+char btn2Pressionado = 0;
+
+unsigned int contagem = 43190;  // 43190s = 11:59:50
+
+unsigned int aux = 0;
+unsigned int segundos = 0;
+unsigned int minutos = 0;
+unsigned int horas = 0;
+
+unsigned int segundosUnidade = 0;
+unsigned int segundosDezena = 0;
+unsigned int minutoUnidade = 0;
+unsigned int minutoDezena = 0;
+unsigned int horaUnidade = 0;
+unsigned int horaDezena = 0;
+
+char estamosContando = 1;
+int tempo = 1;
+char qualDisplay = 4;
+unsigned char auxDosLeds = 0;
+unsigned char maxDosLeds = 250;
+
+void main(void) {
+  TRISA = 0b00000000;
+  TRISE = 0b00000000;
+  TRISD = 0b00000000;
+  ADCON1 = 0x0F;  // Todas Portas digitais
+  CC1 = 0;
+  CC2 = 0;
+  CC3 = 0;
+  CC4 = 0;
+  while (1) {
+    // BOTAO 0
+    if (BTN0 == 0 && btn0Pressionado == 0) {
+      btn0Pressionado = 1;
+      if (estamosContando == 0) contagem += 3600;  // incrementa as horas
+      Delay1KTCYx(DELAY_DOS_BTNS);
+      if (estamosContando) tempo++;
+    } else if (BTN0 == 1 && btn0Pressionado == 1) {
+      btn0Pressionado = 0;
+    }
+
+
+    // BOTAO 1
+    if (BTN1 == 0 && btn1Pressionado == 0) {
+      btn1Pressionado = 1;
+      if (estamosContando == 0) contagem += 60;  // incrementa os minutos
+      Delay1KTCYx(DELAY_DOS_BTNS);
+      if (estamosContando) tempo++;
+    } else if (BTN1 == 1 && btn1Pressionado == 1) {
+      btn1Pressionado = 0;
+    }
+
+
+    // BOTAO 2
+    if (BTN2 == 0 && btn2Pressionado == 0) {
+      btn2Pressionado = 1;
+      if (estamosContando == 0)
+        estamosContando = 1;
+      else
+        estamosContando = 0;
+      Delay1KTCYx(DELAY_DOS_BTNS);
+      if (estamosContando) tempo++;
+    } else if (BTN2 == 1 && btn2Pressionado == 1) {
+      btn2Pressionado = 0;
+    }
+
+
+    if (estamosContando) {
+      if (tempo > 1000) {
+        tempo = 0;
+        contagem++;
+      }
+    }
+    if (contagem == 0) contagem = 43199;  // 43199s = 11:59:59
+    if (contagem > 43199) contagem -= 43199;
+
+
+    // CONVERTER O NUMERO PARA OS DIGITOS INDIVIDUAIS
+    // 1. Converte pra hms
+    aux = contagem;
+    horas = aux / 3600;
+    aux = aux - (horas * 3600);
+    minutos = aux / 60;
+    aux = aux - (minutos * 60);
+    segundos = aux;
+
+    // 2. Converte pros numeros individuais
+    segundosDezena = segundos / 10;
+    segundos -= (segundosDezena * 10);
+    segundosUnidade = segundos;
+
+    minutoDezena = minutos / 10;
+    minutos -= (minutoDezena * 10);
+    minutoUnidade = minutos;
+
+    horaDezena = horas / 10;
+    horas -= (horaDezena * 10);
+    horaUnidade = horas;
+
+
+    /* ATUALIZA OS VALORES DOS DISPLAYS
+        Cada Delay1KTCYx esta configurado para 1ms
+        ou seja, esse processo todo leva 4ms
+    */
+    CC1 = 1;
+    LATD = tab[horaDezena];
+    Delay1KTCYx(DELAY_DOS_LEDS);
+    CC1 = 0;
+
+    CC2 = 1;
+    LATD = tab[horaUnidade];
+    Delay1KTCYx(DELAY_DOS_LEDS);
+    CC2 = 0;
+
+    CC3 = 1;
+    LATD = tab[minutoDezena];
+    // LATD = tab[0];
+    Delay1KTCYx(DELAY_DOS_LEDS);
+    CC3 = 0;
+
+    CC4 = 1;
+    LATD = tab[minutoUnidade];
+    Delay1KTCYx(DELAY_DOS_LEDS);
+    if (estamosContando) tempo += 4;
+    CC4 = 0;
+  }
+}
---
+++ lab04/README.md
@@ -0,0 +1,11 @@
+# Objetivos
+- Familiarização com a programação C18 e suas funções;
+- Praticar o interfaceamento com displays de 7 Segmentos.
+
+# Etapa 1. Relógio com Displays
+Com base no circuito da Figura 1, faça um firmware em C18 que implemente um relógio de 12 horas. Inicie o relógio em 11:59 e 50 segundos e verifique o funcionamento no PICSimLab (ou Proteus) e kit XM118.
+
+# Etapa 2. Ajuste do Relógio
+Usando os botões em RB0 a RB2, realize o ajuste do horário indicado nos displays, sendo:
+- o botão RB2 coloca o relógio em modo de ajuste;
+- os botões RB0 e RB1 incrementam as horas e minutos com um intervalo de tempo razoável para o usuário ajustar.
